7aa2875a6e238eb219cb349da06527a98db0047b
TheTrunk <tadeas@runonflux.io>
2026-04-30 13:06:51 +0800

    remove obsolete

--- bootloader_program/sdk/dist/src/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SQUADS_PROGRAM_ID = void 0;
-const web3_js_1 = require("@solana/web3.js");
-// Constants
-exports.SQUADS_PROGRAM_ID = new web3_js_1.PublicKey('SQDS4ep65T869zMMBKyuUq6aD6EgTu8psMjkvj52pCf');
-//# sourceMappingURL=types.js.map
